02b219f2e6b60704b7d46c75193fb694e67f8e2b
Julio <julio@tide.org>
2023-03-22 15:04:06 +1000

    Add project files.

---
+++ PlatypusPasswords/Helpers/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace PlatypusPasswords.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(Configuration.GetConnectionString("LocalDbConnectionString"));
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+
+    }
+}
